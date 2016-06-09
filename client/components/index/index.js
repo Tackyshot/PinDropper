ad36aa19a59dfd80b351bee8903d368d27ec208d
dillon <dillon@lumenlearning.com>
2016-06-09 13:00:30 -0700

    moved main app to new route '/app';

--- client/components/index/index.js
+++ client/components/index/index.js
@@ -1,78 +1,22 @@
 "use strict";
-import React            from 'react';
-import _                from 'lodash';
-import {
-        Corkboard,
-        CorkboardMenu,
-        CorkboardMenuItem,
-       }                from 'corkboard'
-import {
-        Armor,
-        Inventory,
-        Notes,
-        Skills,
-        Spells,
-        Stats,
-        Weapons
-      }                 from './rightMenuContent/'
-/*
-import ContentWrapper   from '../_common/contentWrapper/';
-import TodoItem         from './todoItem.jsx';
-import TodoActions      from '../../actions/todo-actions.jsx';
-import TodoStore        from '../../stores/todo-store.jsx';
-
-import FlatButton       from 'material-ui/lib/flat-button.js';
-import AddIcon          from 'material-ui/lib/svg-icons/content/add.js';
-*/
+import AppBar   from 'material-ui/lib/app-bar';
+import Style    from './_common/_baseStyle/'
+import React    from 'react';
 
 export default class Index extends React.Component{
   constructor(props, context){
     super(props, context);
 
-    this.state = {};
-
-    //rebindings
-
-  }
-
-  componentWillMount(){
-
+    this.state = {}
   }
 
   render(){
-    return (
-      <Corkboard>
-        <CorkboardMenu>
-          <CorkboardMenuItem menuIcon='viking-helmet-48.png' >
-            <Stats />
-          </CorkboardMenuItem>
+    let style = Style.styles;
 
-          <CorkboardMenuItem menuIcon='sword-48.png' >
-            <Weapons />
-          </CorkboardMenuItem>
+    return(
+      <div className="ApplicationIndex" >
 
-          <CorkboardMenuItem menuIcon='armored-breastplate-48.png' >
-            <Armor />
-          </CorkboardMenuItem>
-
-          <CorkboardMenuItem menuIcon='rucksack-48.png' >
-            <Inventory />
-          </CorkboardMenuItem>
-
-          <CorkboardMenuItem menuIcon='tasks-48.png' >
-            <Skills />
-          </CorkboardMenuItem>
-
-          <CorkboardMenuItem menuIcon='wizard-48.png' >
-            <Spells />
-          </CorkboardMenuItem>
-
-          <CorkboardMenuItem menuIcon='note-48.png' >
-            <Notes />
-          </CorkboardMenuItem>
-        </CorkboardMenu>
-      </Corkboard>
+      </div>
     )
-  }//render
-
+  }
 }
--- client/routes.jsx
+++ client/routes.jsx
@@ -3,8 +3,9 @@ import React                                       from 'react';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 //components
-import Base     from './components/base.jsx';
-import Index    from './components/index/';
+import Base         from './components/base.jsx';
+import Index        from './components/index/';
+import PinDropper   from './components/pindropper';
 
 export default () => {
 
@@ -12,6 +13,7 @@ export default () => {
     <Router history={browserHistory}>
       <Route path="/" component={Base}>
         <IndexRoute component={Index} />
+        <Route path='app' component={PinDropper} />
       </Route>
     </Router>
   )
