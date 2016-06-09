0e22f9695236a15607b92d69b77f79e86002139f
Dillon Osman <dillonosman@gmail.com>
2016-06-09 08:11:58 -0700

    login.js has been connected to the db

--- server/routes/route_definitions/login.js
+++ server/routes/route_definitions/login.js
@@ -1,6 +1,10 @@
 "use strict";
-const fs  = require('fs');
-const dir = __dirname + '/../../assets/client/views/login.html';
+const fs        = require('fs');
+const ConnectDb = require('../../assets/helpers/connectdb');
+const Bcrypt    = require('../../assets/helpers/bcrypt');
+const Account   = require('../../assets/models/account');
+const dir       = __dirname + '/../../assets/client/views/login.html';
+
 module.exports = {
   method: ['GET', 'POST'],
   path: "/login",
@@ -13,7 +17,48 @@ module.exports = {
         redirectTo: false
       }
     },
-    handler: (request, reply)=>{
+    handler: (req, res)=> {
+      if (req.auth.isAuthenticated) {
+        return res.redirect('/');
+      }
+
+      switch (req.method) {
+        case 'post':
+          let payload = req.payload;
+          let connectdb = new ConnectDb();
+
+          connectdb.connect((done)=> {
+            Account.findOne({username: payload.username}, (err, account)=> {
+              if (err) console.log('REGISTER ERR:', err);
+              if (account === null) return res(fs.readFileSync(dir)).type('text/html');
+
+              Bcrypt.compare(payload.password, account.password, (isSamePw)=>{
+                if(isSamePw){
+                  req.server.app.cache.set(String(account._id), { account: account }, 0, (err) => {
+                    if (err) return res(err);
+
+                    req.cookieAuth.set({ sid: account._id });
+                    return res.redirect('/');
+                  });
+                }
+                else{
+                  return res(fs.readFileSync(dir)).type('text/html');
+                }
+                done();
+              }); //bcrypt compare
+            }); //account find
+          }); //dbconnect
+        break;
+
+        case 'get':
+          //simpply send the data
+          return res(fs.readFileSync(dir))
+            .type('text/html');
+        break;
+      };
+    },
+    /*handla: (request, reply)=>{
+      /!*EVERYTHING AFTER THIS LINE WILL BE DESTROYED*!/
       const users = {
         john: {
           id: 'dillbill',
@@ -52,12 +97,6 @@ module.exports = {
 
         return reply(fs.readFileSync(dir))
           .type('text/html');
-        /*return reply('<html><head><title>Login page</title></head><body>' +
-          (message ? '<h3>' + message + '</h3><br/>' : '') +
-          '<form method="post" action="/login">' +
-          'Username: <input type="text" name="username"><br>' +
-          'Password: <input type="password" name="password"><br/>' +
-          '<input type="submit" value="Login"></form></body></html>');*/
       }
 
       const sid = String(++uuid);
@@ -71,6 +110,6 @@ module.exports = {
         return reply.redirect('/');
       });
 
-    }
+    }*/
   }
 };
